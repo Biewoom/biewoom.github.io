e65fd7763efa72e4214cb91fb1c797508f4d00e7
JeongminYu <likemin0142@gmail.com>
2020-04-03 14:27:41 +0900

    append _site

---
+++ _site/assets/javascripts/lunr/lunr.store.js
@@ -0,0 +1,11 @@
+var store = [{
+        "title": "GOOD",
+        "excerpt":"IT IS READLL TEST ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/2020/04/03/GOOD.html"
+      },{
+        "title": "Test2",
+        "excerpt":"GOOOOOOOOOOOD ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/2020/04/03/Test2.html"
+      }]
